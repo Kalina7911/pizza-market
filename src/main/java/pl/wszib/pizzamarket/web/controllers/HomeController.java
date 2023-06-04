e62fe74536f062d94c0f42022fa4203e1ac4c403
Alina <kotnarow@student.wszib.edu.pl>
2023-06-04 07:54:11 +0200

    Strona Główna

---
+++ src/main/java/pl/wszib/pizzamarket/web/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package pl.wszib.pizzamarket.web.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String homePage (Model model){
+        model.addAttribute("firstName", "ala");
+        return "homePage";
+    }
+}
